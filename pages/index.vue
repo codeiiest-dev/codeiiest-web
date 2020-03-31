d21614c0bee5aff53a385c1fd208a54cb3c53ca0
Dwaipayan Ray <dwaipayanray1@gmail.com>
2020-03-31 17:02:34 +0530

    Init

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Hero />
+  </div>
+</template>
+
+<script>
+import Hero from '~/components/Landing/Hero.vue'
+
+export default {
+  components: {
+    Hero
+  }
+}
+</script>
+
+<style>
+</style>
