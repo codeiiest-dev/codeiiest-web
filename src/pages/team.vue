fe35edca71d42b07c398f7a27977d5e9584a9c38
Arnab Sen <arnabsen1729@gmail.com>
2021-06-29 20:17:11 +0530

    upd: Added Github icon

--- src/pages/team.vue
+++ src/pages/team.vue
@@ -87,19 +87,20 @@ const members: Array<{name: string; role: string; avatar: string; socials: Objec
                   duration-300
                   fade
                 "
-                src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=facearea&amp;facepad=8&amp;w=1024&amp;h=1024&amp;q=80"
+                :src="member.avatar"
                 alt=""
               />
               <div class="space-y-2">
                 <div class="text-lg leading-6 font-medium space-y-1">
                   <h3>{{ member.name }}</h3>
                   <p class="text-red-600">
-                    Copywriter
+                    {{ member.role }}
                   </p>
                 </div>
                 <ul class="flex justify-center space-x-5">
                   <li>
-                    <a href="#" class="text-gray-400 hover:text-gray-500">
+                    <iconify-icon data-icon="noto:waving-hand"></iconify-icon>
+                    <a :href="member.socials.twitter" class="text-gray-400 hover:text-gray-500">
                       <span class="sr-only">Twitter</span>
                       <svg
                         class="w-5 h-5"
@@ -113,8 +114,9 @@ const members: Array<{name: string; role: string; avatar: string; socials: Objec
                       </svg>
                     </a>
                   </li>
+
                   <li>
-                    <a href="#" class="text-gray-400 hover:text-gray-500">
+                    <a :href="member.socials.linkedin" class="text-gray-400 hover:text-gray-500">
                       <span class="sr-only">LinkedIn</span>
                       <svg
                         class="w-5 h-5"
@@ -128,6 +130,18 @@ const members: Array<{name: string; role: string; avatar: string; socials: Objec
                           clip-rule="evenodd"
                         ></path>
                       </svg>
+                    </a>
+                  </li>
+                  <li>
+                    <a :href="member.socials.github" class="text-gray-400 hover:text-gray-500">
+                      <span class="sr-only">Github</span>
+                      <svg
+                        class="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                        aria-hidden="true"
+                      ><path d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33c.85 0 1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2z" fill="currentColor"></path></svg>
+
                     </a>
                   </li>
                 </ul>
