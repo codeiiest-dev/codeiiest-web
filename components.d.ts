a24df427ada93ff4cc4eec88bebf7dafe7066733
Arnab Sen <arnabsen1729@gmail.com>
2021-06-30 00:12:46 +0530

    fix: color correction

--- components.d.ts
+++ components.d.ts
@@ -4,16 +4,15 @@
 declare module 'vue' {
   export interface GlobalComponents {
     Brand: typeof import('./src/components/Brand.vue')['default']
+    BrandDark: typeof import('./src/components/BrandDark.vue')['default']
     Header: typeof import('./src/components/Header.vue')['default']
     README: typeof import('./src/components/README.md')['default']
-    BrandDark: typeof import('./src/components/BrandDark.vue')['default']
-    CarbonMoon: typeof import('virtual:vite-icons/carbon/moon')['default']
-    CarbonSun: typeof import('virtual:vite-icons/carbon/sun')['default']
     PopoverButton: typeof import('@headlessui/vue')['PopoverButton']
     PopoverPanel: typeof import('@headlessui/vue')['PopoverPanel']
     Popover: typeof import('@headlessui/vue')['Popover']
     PopoverGroup: typeof import('@headlessui/vue')['PopoverGroup']
-    CarbonDicomOverlay: typeof import('virtual:vite-icons/carbon/dicom-overlay')['default']
+    CarbonMoon: typeof import('virtual:vite-icons/carbon/moon')['default']
+    CarbonSun: typeof import('virtual:vite-icons/carbon/sun')['default']
   }
 }
 
