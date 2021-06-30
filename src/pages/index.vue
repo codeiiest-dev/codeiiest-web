2adbd6e64ade917e973aa97088ebeb6a60d14ab4
Arnab Sen <arnabsen1729@gmail.com>
2021-06-30 09:53:07 +0530

    fix: spacing fixed

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -5,17 +5,17 @@
       <!-- space -->
       <span class="block text-red-600 xl:inline">IIEST</span>
     </h1>
-    <p class="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+    <p class="mt-3 max-w-md mx-auto text-base text-gray-500 dark:text-gray-400 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
       The Coding Club of IIEST, Shibpur.
       <br><br>
       Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, corrupti beatae! Odio facilis consectetur veniam recusandae dolor blanditiis aut, sint tempora excepturi quod qui maiores perferendis alias laboriosam eum voluptate?
     </p>
-    <div class="mt-10">
+    <div class="mt-15">
       <h1 class="dark:text-gray-100 text-gray-900 text-3xl tracking-tight font-bold">
         Chapters
       </h1>
-      <div class="flex flex-wrap justify-center">
-        <div v-for="(chapter, index) in chapters" :key="index" class="w-full max-w-sm px-4 py-3 mx-5 my-3 bg-white rounded-md shadow-md dark:bg-gray-800">
+      <div class="flex flex-wrap justify-center mt-5">
+        <div v-for="(chapter, index) in chapters" :key="index" class="w-full max-w-sm px-4 py-3 mx-5 my-3 bg-white rounded-md shadow-md hover:shadow-lg dark:bg-dark-800">
           <div>
             <h1 class="mt-2 text-lg font-semibold text-gray-800 dark:text-white">
               {{ chapter.name }}
@@ -27,7 +27,7 @@
 
           <div>
             <div class="flex items-center mt-2 text-gray-700 dark:text-gray-200">
-              <a :href="chapter.route" class="mx-2 text-blue-600 cursor-pointer dark:text-blue-400 hover:underline">Read More</a>
+              <a :href="chapter.route" class="mx-2 text-red-600 cursor-pointer dark:text-red-400 hover:underline">Read More</a>
             </div>
           </div>
         </div>
