4b9c7f17ac95c79e36b3ab6c6e50b2f6b87499f0
Anik Das <anikdas0811@gmail.com>
2021-07-02 13:51:56 +0530

    chore(lint): fix linting and removed useless code
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,4 +1,10 @@
 <template>
-  <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FKolkata&amp;src=NXJidmI0azdjMTB1amZycGVib2g1amUwNzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%234285F4&amp;title=CodeIIEST%20Sessions" style="border:solid 1px #777" width="800" height="600" frameborder="0"
-scrolling="no"></iframe>
+  <iframe
+    src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;ctz=Asia%2FKolkata&amp;src=NXJidmI0azdjMTB1amZycGVib2g1amUwNzRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%234285F4&amp;title=CodeIIEST%20Sessions"
+    style="border:solid 1px #777"
+    width="800"
+    height="600"
+    frameborder="0"
+    scrolling="no"
+  ></iframe>
 </template>
