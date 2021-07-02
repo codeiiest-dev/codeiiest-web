176c2b38a8ba9347eaefeb562e3ccfcbb30610b5
Rahul Halder <hrahul2605@gmail.com>
2021-07-02 13:21:07 +0530

    add: format date method

--- src/logic/events.ts
+++ src/logic/events.ts
@@ -1,7 +1,14 @@
 import axios from 'axios'
+import dayjs from 'dayjs'
+
+const DATE_FORMAT = 'DD MMM, YYYY'
 
 export const getEvents = async(): Promise<any> => {
   const { data } = await axios.get('https://www.googleapis.com/calendar/v3/calendars/5rbvb4k7c10ujfrpeboh5je074@group.calendar.google.com/events?key=AIzaSyDYjDZLMD5FgoTZwfLcqkdMlen3MwceA84')
 
   return data
+}
+
+export const formatDateTime = (dateTime: string): string => {
+  return dayjs(dateTime).format(DATE_FORMAT)
 }
