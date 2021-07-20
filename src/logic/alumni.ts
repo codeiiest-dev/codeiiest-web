d9cdc5241b17c89512086dc11d3d0b2b2bf32420
Arnab Sen <arnabsen1729@gmail.com>
2021-07-21 01:06:29 +0530

    Add alumni data

--- src/logic/alumni.ts
+++ src/logic/alumni.ts
@@ -1,17 +1,215 @@
-export const alumniMembers: Array<{name: string; desc: string; avatar: string; twitter?: string; linkedin?: string; github?: string}> = [
+export const alumniMembers: Array<{
+    name: string;
+    avatar: string;
+    link: {
+        github?: string;
+        linkedin?: string;
+        twitter?: string;
+        facebook?: string;
+    };
+}> = [
     {
+        avatar: '/alumni/suri.png',
+        link: { github: 'https://github.com/suri-kumkaran' },
+        name: 'Suryansh Gupta',
+    },
+    {
+        avatar: '/alumni/arijit.png',
+        link: { github: 'https://github.com/TheArijitSaha' },
+        name: 'Arijit Saha',
+    },
+    {
+        avatar: '/alumni/MD.png',
+        link: {
+            facebook: 'https://www.facebook.com/mdazmat786',
+            linkedin: 'https://linkedin.com/in/mdazmat9/',
+        },
+        name: 'Md Azmat Ali',
+    },
+    {
+        avatar: '/alumni/nindo.png',
+        link: {
+            facebook: 'https://www.facebook.com/nindo4',
+            linkedin: 'https://www.linkedin.com/in/anindya-bhowmick-a8341b180/',
+        },
+        name: 'Anindya Bhowmick',
+    },
+    {
+        avatar: '/alumni/arsh.png',
+        link: {
+            facebook: 'https://www.facebook.com/arsh.trehan',
+            linkedin: 'https://www.linkedin.com/in/arsh-trehan-993996157/',
+        },
+        name: 'Arsh Trehan',
+    },
+    {
+        avatar: '/alumni/vivek.png',
+        link: {
+            facebook: 'https://www.facebook.com/vivek.maurya.96592',
+            linkedin: 'https://www.linkedin.com/in/vivek-maurya-41793b182/',
+        },
+        name: 'Vivek Maurya',
+    },
+    {
+        avatar: '/alumni/dwai.png',
+        link: {
+            facebook: 'https://www.facebook.com/profile.php?id=100009905534262',
+            linkedin: 'https://www.linkedin.com/in/dwaipayan-ray/',
+        },
         name: 'Dwaipayan Ray',
-        desc: 'SDE Intern @CodeChef',
-        avatar: 'https://avatars.githubusercontent.com/u/35661311?v=4'
     },
     {
-        name: 'Shivam Kuman Singh',
-        desc: 'SDE Intern @Microsoft',
-        avatar: 'https://avatars.githubusercontent.com/u/47945055?v=4'
+        avatar: '/alumni/shivam.png',
+        link: {
+            facebook: 'https://www.facebook.com/heyshivam51',
+            linkedin: 'https://www.linkedin.com/in/shivamhere/',
+        },
+        name: 'Shivam Kumar Singh',
     },
     {
-        name: 'Srikanth Narapureddy',
-        desc: 'SWE Intern @Google',
-        avatar: 'https://avatars.githubusercontent.com/u/63089621?v=4'
-    }
-]
+        avatar: '/alumni/srikant.png',
+        link: { linkedin: 'https://www.linkedin.com/in/narapureddy-srikanth/' },
+        name: 'Srikanth N.',
+    },
+    {
+        avatar: '/alumni/ankit.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/peace.247ankit',
+            linkedin: 'https://www.linkedin.com/in/ankit-kumar-giri-760805165/',
+        },
+        name: 'Ankit Kumar Giri',
+    },
+    {
+        avatar: '/alumni/rahul.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/peace.247ankit',
+            linkedin: 'https://www.linkedin.com/in/ankit-kumar-giri-760805165/',
+        },
+        name: 'Rahul Indra',
+    },
+    {
+        avatar: '/alumni/ankitt.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/ankit.taxak.9',
+            github: 'https://github.com/ankittaxak5717',
+        },
+        name: 'Ankit Taxak',
+    },
+    {
+        avatar: '/alumni/prateek.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/prateek.chanda.1',
+            github: 'https://github.com/prateekiiest',
+            linkedin: 'https://www.linkedin.com/in/prateek-chanda-iiest/',
+        },
+        name: 'Prateek Chanda',
+    },
+    {
+        avatar: '/alumni/soubhik.jpeg',
+        link: { github: 'https://github.com/soubh1k' },
+        name: 'Soubhik Rakshit',
+    },
+    {
+        avatar: '/alumni/virali.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/virali.vora.9',
+            github: 'https://github.com/viralivora',
+        },
+        name: 'Virali Vora',
+    },
+    {
+        avatar: '/alumni/rup.jpg',
+        link: {
+            github: 'https://github.com/Rupkatha',
+            linkedin: 'https://www.linkedin.com/in/rupkatha-hira-865b59150/',
+        },
+        name: 'Rupkatha Hira',
+    },
+    {
+        avatar: '/alumni/ujjwal.jpg',
+        link: { facebook: 'https://www.facebook.com/ujjwal.sudeo' },
+        name: 'Ujjwal Sudev',
+    },
+    {
+        avatar: '/alumni/shree.jpg',
+        link: { facebook: 'https://www.facebook.com/shreeyant' },
+        name: 'Shreeyant Sinha',
+    },
+    {
+        avatar: '/alumni/manish.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/mk.tesla',
+            linkedin: 'https://www.linkedin.com/in/manish-kumar-276511148/',
+        },
+        name: 'Manish Kumar',
+    },
+    {
+        avatar: '/alumni/anunay.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/anunay.kumar.5205',
+            linkedin: 'https://www.linkedin.com/in/anunay-kumar-16b7b9152/',
+        },
+        name: 'Anunay Kumar',
+    },
+    {
+        avatar: '/alumni/himanshu.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/profile.php?id=100007296848247',
+            linkedin: 'https://www.linkedin.com/in/himanshu-pandey-87144012a/',
+        },
+        name: 'Himanshu Pandey',
+    },
+    {
+        avatar: '/alumni/mohanee.jpg',
+        link: { facebook: 'https://www.facebook.com/mohanee.dammu' },
+        name: 'Mohanee Dammu',
+    },
+    {
+        avatar: '/alumni/mayank.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/darkcode.manumeral',
+            linkedin: 'https://www.linkedin.com/in/mayank8/',
+        },
+        name: 'Mayank Kumar',
+    },
+    {
+        avatar: '/alumni/sandeep.jpg',
+        link: { facebook: 'https://www.facebook.com/sandeep.gprs' },
+        name: 'Sandeep Kumar',
+    },
+    {
+        avatar: '/alumni/ankitch.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/ankitchoudhary2008',
+            linkedin: 'https://www.linkedin.com/in/ankit-choudhary-b294a532/',
+        },
+        name: 'Ankit Choudhary',
+    },
+    {
+        avatar: '/alumni/nilanjana.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/nilanjanalodh.trisha',
+            linkedin: 'https://www.linkedin.com/in/nilanjana-lodh/',
+        },
+        name: 'Nilanjana Lodh',
+    },
+    {
+        avatar: '/alumni/arun.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/arunkr9088',
+            github: 'https://github.com/arunKumarNOP',
+            linkedin:
+                'https://www.linkedin.com/in/arun-kumar-shreevastava-77b305136/',
+        },
+        name: 'Arun Shreevastava',
+    },
+    {
+        avatar: '/alumni/sapt.jpg',
+        link: {
+            facebook: 'https://www.facebook.com/saptarashmi.bandyopadhyay',
+            linkedin:
+                'https://www.linkedin.com/in/saptarashmi-bandyopadhyay-272999ba/',
+        },
+        name: 'Saptarashmi Bandyopadhyay',
+    },
+];
