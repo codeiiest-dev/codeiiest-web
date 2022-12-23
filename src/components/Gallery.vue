1eeb3ea33e2f7e1dd92c0f7fce0dd688bbbd05c1
Arnab Sen <arnabsen1729@gmail.com>
2022-12-24 02:24:50 +0530

    feat: add a new gallery page

---
+++ src/components/Gallery.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="gallery-container">
+        <div class="gallery-item" v-for="(image, index) in images" :key="index">
+            <img :src="image" alt="random image">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Gallery',
+    data() {
+        return {
+            images: []
+        }
+    },
+    methods: {
+        getGalleryImages() {
+            const images = import.meta.glob('../../public/gallery/*.*')
+            for (const path in images) {
+                this.images.push(path.replace('../../public', ''));
+            }
+        },
+    },
+    mounted() {
+        this.getGalleryImages();
+        console.log(this.images);
+    },
+}
+</script>
